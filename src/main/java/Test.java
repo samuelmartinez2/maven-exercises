6967684698cd5817a7c077c58197e6f95ce75ce6
sam's macbook <samuel2.martinez2@gmail.com>
2022-07-01 12:25:16 -0500

    dependency exercise

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,13 +1,35 @@
-import org.apache.commons.collections4.MultiSet;
-import org.apache.commons.collections4.multiset.HashMultiSet;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Test {
-    MultiSet<String> multiset = new HashMultiSet<>() {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Something:");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: "+ userInput);
+        String reversedStatement = StringUtils.reverse(userInput);
 
-        }
+        if( StringUtils.isNumeric(userInput)){
+            System.out.println(userInput+" is a number");
+        } else System.out.println(" is not a number");
+
+        if (StringUtils.isAllUpperCase(userInput)) {
+            System.out.println(StringUtils.lowerCase(userInput));
+            String lowCase= StringUtils.lowerCase(userInput);
+            System.out.println(lowCase + " has been turned to lowerCase");
+
+        } else
+            System.out.printf(StringUtils.upperCase(userInput+  " has been turned to UPPERCASE"));
+
+
+
+
+
+
+
+    }
     }
